90c95dedc13d390662d6b1b6cbbf9d574c18b751
Jess-Jiaqi <li.jiaqi5@northeastern.edu>
2024-02-26 17:00:14 -0800

    Initial Commit

---
+++ DesignIOS15/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DesignIOS15
+//
+//  Created by Jiaqi Li on 2/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DesignIOS15/DesignIOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignIOS15App.swift
+//  DesignIOS15
+//
+//  Created by Jiaqi Li on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct DesignIOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
