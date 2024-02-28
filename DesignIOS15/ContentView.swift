6b09e01b5c9a8d2f816662bc927e662364918dcd
Jess-Jiaqi <li.jiaqi5@northeastern.edu>
2024-02-27 22:45:21 -0800

    Home card

--- DesignIOS15/ContentView.swift
+++ DesignIOS15/ContentView.swift
@@ -9,13 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading, spacing: 8.0) {
+            Spacer()
+            Image("Logo 2")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 26.0, height: 26.0)
+                .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+            Text("SwiftUI for IOS 15")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            Text("20 sections - 3 hours")
+                .font(.footnote)
+                .fontWeight(.semibold)
+            Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
+                .font(.footnote)
+                .multilineTextAlignment(.leading)
+                .lineLimit(2)
         }
-        .padding()
+        .padding(.all, 20.0)
+        .frame(height: 350.0)
+        .background(Color("Background"))
+        .cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
+        .shadow(radius: /*@START_MENU_TOKEN@*/20/*@END_MENU_TOKEN@*/)
     }
 }
 
