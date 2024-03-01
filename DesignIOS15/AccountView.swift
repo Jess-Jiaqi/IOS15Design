f852fd2571f4d12dafae4b4301deaa3bf0ec2317
Jess-Jiaqi <li.jiaqi5@northeastern.edu>
2024-02-29 23:39:04 -0800

    add AccountView

---
+++ DesignIOS15/AccountView.swift
@@ -0,0 +1,18 @@
+//
+//  AccountView.swift
+//  DesignIOS15
+//
+//  Created by Jiaqi Li on 2/29/24.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccountView()
+}
